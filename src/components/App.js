b1c0a80ca19bb78f41b3bbd6050ec73879910daa
jensan97 <jennysanchez97@icloud.com>
2022-02-06 01:45:43 -0800

    command

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+
+import {
+    Time
+ } from './index.js'
+
+const App = () => {
+    
+    
+}
+
+
+   
+
+
+
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export {default as App} from './App'
+//export {default as Time} from './Time'
+import'../'node_modules/bootstrap/dist/css/bootstrap.css' ; 
